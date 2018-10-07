d1a4529c34a3ebcbfa9c05d7b76eb9b077abe597
FernandoLeall <43920938+FernandoLeall@users.noreply.github.com>
2018-10-07 00:28:54 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,4 +0,0 @@
-#calcule o salário do funcionário com 15% de aumento
-salario = float(input('Qual é o salário atual do funcionário? R$ '))
-novo = salario + (salario * 15 / 100)
-print('O funcionário que recebia R$ {:.2f}, com 15% de aumento, passa a receber R$ {:.2f}'.format(salario, novo))
